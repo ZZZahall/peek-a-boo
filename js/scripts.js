d6c585d1d3dea144a561c0c43c09ef52bb4c8e29
Stella Huayhuaca and Zahall Mahmood <stella@>
2017-01-10 12:02:06 -0800

    highlight p js for walrus

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,20 @@ $(document).ready(function() {
     $("#walrus-hidden").fadeIn();
     $("#walrus-showing").fadeOut();
   });
+  $("button#dark").click(function() {
+    $("body").removeClass();
+    $("body").addClass("dark-background");
+  });
+  $("button#light").click(function() {
+    $("body").removeClass();
+    $("body").addClass("light-background");
+  });
+  $("button#original").click(function() {
+    $("body").removeClass();
+  });
+  $("p").click(function() {
+    $("p").addClass("highlight");
+    $('p').addClass("bg-success");
+  });
+
 });
